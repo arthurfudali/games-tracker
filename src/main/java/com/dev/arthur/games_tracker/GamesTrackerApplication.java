c5ae8034349fd2cf838b006b0b4f2ec401b53c1f
arthurfudali <arthur.fudali@gmail.com>
2025-11-11 20:20:20 -0300

    Initial commit

---
+++ src/main/java/com/dev/arthur/games_tracker/GamesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.arthur.games_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/arthur/games_tracker/GamesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.arthur.games_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
