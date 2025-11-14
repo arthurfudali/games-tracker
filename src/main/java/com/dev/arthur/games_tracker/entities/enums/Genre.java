55ac38a677f3a422b9d4b2d69c770f49bd641d17
arthurfudali <arthur.fudali@gmail.com>
2025-11-13 21:29:42 -0300

    feat: create Genre Enum

---
+++ src/main/java/com/dev/arthur/games_tracker/entities/enums/Genre.java
@@ -0,0 +1,11 @@
+package com.dev.arthur.games_tracker.entities.enums;
+
+public enum Genre {
+    RPG,
+    PUZZLE,
+    MUNDO_ABERTO,
+    EXPLORACAO,
+    INDIE,
+    INVESTIGACAO,
+    BOSS_HUNT
+}
