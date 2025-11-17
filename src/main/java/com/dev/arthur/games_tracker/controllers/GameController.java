f2cea4355825277d2f05c6621bcca87851f805ef
arthurfudali <arthur.fudali@gmail.com>
2025-11-17 17:55:49 -0300

    refactor: refactor GameController

--- src/main/java/com/dev/arthur/games_tracker/controllers/GameController.java
+++ src/main/java/com/dev/arthur/games_tracker/controllers/GameController.java
@@ -1,24 +1,54 @@
 package com.dev.arthur.games_tracker.controllers;
 
 import com.dev.arthur.games_tracker.controllers.dtos.BestDealDTO;
+import com.dev.arthur.games_tracker.entities.Game;
+import com.dev.arthur.games_tracker.services.GameAPIService;
 import com.dev.arthur.games_tracker.services.GameService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/games")
 public class GameController {
+    private final GameAPIService gameAPIService;
     private final GameService gameService;
 
-    public GameController(GameService gameService) {
+    public GameController(GameAPIService gameAPIService, GameService gameService) {
+        this.gameAPIService = gameAPIService;
         this.gameService = gameService;
     }
 
     @GetMapping("/bestprice/{title}")
     public Mono<BestDealDTO> getBestPrice(@PathVariable String title) {
-        return gameService.findBestDealByTitle(title);
+        return gameAPIService.findBestDealByTitle(title);
     }
+
+    @PostMapping
+    public Game create(@RequestBody Game game) {
+        return gameService.saveGame(game);
+    }
+
+    @GetMapping
+    public List<Game> list() {
+        return gameService.getGames();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Game> get(@PathVariable String id) {
+        return gameService.getGame(id);
+    }
+
+    @PutMapping("/{id}")
+    public Game update(@PathVariable String id, @RequestBody Game game) {
+        return gameService.updateGame(game);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        gameService.deleteGame(id);
+    }
+
 }
