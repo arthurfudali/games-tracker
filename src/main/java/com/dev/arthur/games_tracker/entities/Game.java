0904e539f7e629e39431a9825ace16e40b68110e
arthurfudali <arthur.fudali@gmail.com>
2025-11-18 19:41:40 -0300

    fix: typo in Game.java

--- src/main/java/com/dev/arthur/games_tracker/entities/Game.java
+++ src/main/java/com/dev/arthur/games_tracker/entities/Game.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collation = "games")
+@Document(collection = "games")
 public class Game {
     @Id
     private String id;
