2046bf556ba6a49d9d0205f58dedea0b90e834a2
arthurfudali <arthur.fudali@gmail.com>
2025-11-18 20:57:54 -0300

    feat: Add endpoint to get best price by game ID

--- src/main/java/com/dev/arthur/games_tracker/controllers/GameController.java
+++ src/main/java/com/dev/arthur/games_tracker/controllers/GameController.java
@@ -2,6 +2,7 @@ package com.dev.arthur.games_tracker.controllers;
 
 import com.dev.arthur.games_tracker.controllers.dtos.BestDealDTO;
 import com.dev.arthur.games_tracker.entities.Game;
+import com.dev.arthur.games_tracker.repositories.GameRepository;
 import com.dev.arthur.games_tracker.services.GameAPIService;
 import com.dev.arthur.games_tracker.services.GameService;
 import org.springframework.web.bind.annotation.*;
@@ -15,15 +16,26 @@ import java.util.Optional;
 public class GameController {
     private final GameAPIService gameAPIService;
     private final GameService gameService;
+    private final GameRepository gameRepository;
 
-    public GameController(GameAPIService gameAPIService, GameService gameService) {
+    public GameController(GameAPIService gameAPIService, GameService gameService, GameRepository gameRepository) {
         this.gameAPIService = gameAPIService;
         this.gameService = gameService;
+        this.gameRepository = gameRepository;
     }
 
-    @GetMapping("/bestprice/{title}")
-    public Mono<BestDealDTO> getBestPrice(@PathVariable String title) {
+    @GetMapping("/bestprice/title/{title}")
+    public Mono<BestDealDTO> getBestPriceByTitle(@PathVariable String title) {
         return gameAPIService.findBestDealByTitle(title);
+    }
+
+    @GetMapping("/bestprice/id/{id}")
+    public Mono<BestDealDTO> getBestPriceById(@PathVariable String id) {
+        Game game = gameRepository.findById(id).orElse(null);
+        if (game == null) {
+            return Mono.empty();
+        }
+        return gameAPIService.findBestDealByTitle(game.getTitle());
     }
 
     @PostMapping
