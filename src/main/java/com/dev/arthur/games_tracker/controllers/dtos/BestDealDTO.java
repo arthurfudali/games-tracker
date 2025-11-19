a781c178be16ed77aecfc6a9592d3cf77f4e0325
arthurfudali <arthur.fudali@gmail.com>
2025-11-19 19:48:21 -0300

    refactor: BestDealDTO to use Java record

--- src/main/java/com/dev/arthur/games_tracker/controllers/dtos/BestDealDTO.java
+++ src/main/java/com/dev/arthur/games_tracker/controllers/dtos/BestDealDTO.java
@@ -1,15 +1,4 @@
 package com.dev.arthur.games_tracker.controllers.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class BestDealDTO {
-    private String storeName;
-    private double price;
-    private String url;
-
+public record BestDealDTO(String storeName, double price, String url) {
 }
